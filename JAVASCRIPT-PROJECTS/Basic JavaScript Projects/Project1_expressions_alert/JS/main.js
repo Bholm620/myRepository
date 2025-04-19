476bb51265302bb3a2eabb6329fa1f32d4b2d1c9
Brandon Holm <Bholm866@gmail.com>
2025-04-18 20:45:29 -0600

    Added another modification to file1

---
+++ JAVASCRIPT-PROJECTS/Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+alert ("Hello, World!");
+
+
+
+document.write("Goodbye, World!");
+
+var x = "5";
+
+document.write(5);
+
+
+
+window.alert(x);
+
+document.write("I told ypu, \"No, way\"");
+
+var x="Part\"one\"," + "part \"two\"";
+document.write(x);
+
+var Dad = "Andrew"; Mom="Mary"; //who?
+document.write(Dad);
